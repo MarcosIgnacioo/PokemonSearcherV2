f108ff6ec73c9db715a15fbb22daeb22884bc35e
Marcos <magma_ruby145@hotmail.com>
2023-07-07 21:58:40 -0700

    api gets pokemon

--- src/main/java/pokemon/classes/TypePokemon.java
+++ src/main/java/pokemon/classes/TypePokemon.java
@@ -24,7 +24,6 @@ public class TypePokemon {
     public void setName(String name) {
         this.name = name;
     }
-
     @Override
     public String toString() {
         return "["+ name + "]";
